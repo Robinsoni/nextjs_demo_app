decdc5722c1cdc8629ffc88d797e698b525dc304
Robin Soni <sonirobin369@gmail.com>
2024-11-16 23:12:06 +0530

    delaying the page components response for testing the suspense effect.

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -2,11 +2,13 @@ import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '../../ui/font';
-import { fetchCardData, fetchLatestInvoices, fetchRevenue } from '../../lib/data';
+import { fetchCardData, /* fetchLatestInvoices ,*/ /* fetchRevenue */ } from '../../lib/data';
+import { Suspense } from 'react';
+import { LatestInvoicesSkeleton, RevenueChartSkeleton } from '@/app/ui/skeletons';
  
 export default async function Page() {
-  const revenue = await fetchRevenue();
-  const latestInvoices = await fetchLatestInvoices();
+  /* const revenue = await fetchRevenue(); */
+ /*  const latestInvoices = await fetchLatestInvoices(); */
   const {numberOfInvoices,numberOfCustomers,totalPaidInvoices,totalPendingInvoices} =  await fetchCardData();
   return (
     <main>
@@ -24,8 +26,10 @@ export default async function Page() {
         />}
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        {<RevenueChart revenue={revenue}  />}
-        {<LatestInvoices latestInvoices={latestInvoices} />}
+       {/*  {<RevenueChart revenue={revenue}  />} */}
+       <Suspense fallback={<RevenueChartSkeleton />}><RevenueChart /></Suspense>
+        {/* {<LatestInvoices latestInvoices={latestInvoices} />} */}
+       <Suspense fallback={<LatestInvoicesSkeleton />}><LatestInvoices /></Suspense>
       </div>
     </main>
   );
